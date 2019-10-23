d08e42bbf5fe15a1a06a725dd1596cf23a366c56
unknown <154072@vdi.immall.co.kr>
2019-10-23 16:04:19 +0900

    webpack 초기등록

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const ts = require('gulp-typescript');
+const webpack = require('webpack-stream');
 
 
-gulp.task('test', function() {
+gulp.task('babel', function() {
   var tsconfig = require("./tsconfig.json");
   return gulp.src('src/**/*.ts')
     .pipe(ts(tsconfig.compilerOptions))
@@ -12,4 +13,10 @@ gulp.task('test', function() {
     .pipe(gulp.dest('out'));
 });
 
-
+gulp.task("webpack",function(){
+  return gulp.src("out/**/student.js")
+    .pipe(webpack({output: {
+                 filename: 'App.bundle.js'
+             }}))
+    .pipe(gulp.dest("dist"));
+});
